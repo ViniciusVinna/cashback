21bd8e1eda92b224a13ee2122d2fea5cf6c4a688
Vinícius Ribeiro <vinicius.vrf@gmail.com>
2020-09-19 20:55:46 -0300

    ♻️ Move service worker to config folder

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 
 import App from 'containers/App';
 
-import * as serviceWorker from './serviceWorker';
+import * as serviceWorker from './config/serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
