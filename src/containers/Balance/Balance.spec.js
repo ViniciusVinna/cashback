9b7bc7bb4228fc6c947d8bf36034669a1dcfa301
Vinícius Ribeiro <vinicius.vrf@gmail.com>
2020-09-20 14:26:45 -0300

    ✅ Add tests to Balance container

---
+++ src/containers/Balance/Balance.spec.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import Balance from './Balance';
+
+describe('Balance', () => {
+  it('it should render properly', () => {
+    const { container } = render(
+      <Balance 
+        balance="R$ 450,00"
+        approved="R$ 72,00"
+        pending="R$ 45,00"
+      />
+    );
+    expect(container).toMatchSnapshot();
+  });
+});
