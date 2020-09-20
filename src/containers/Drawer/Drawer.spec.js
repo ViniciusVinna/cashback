394f095b09dfe5402ce92472d1d4c98960e1f624
Vinícius Ribeiro <vinicius.vrf@gmail.com>
2020-09-19 23:07:57 -0300

    ✅ Add Drawer tests

--- src/containers/Drawer/Drawer.spec.js
+++ src/containers/Drawer/Drawer.spec.js
@@ -1,37 +1,18 @@
 import React from 'react';
 import Drawer from './Drawer';
 
-const mockDispatch = jest.fn();
-
 const props = {
   children: <div>child</div>,
-  isDrawerActive: true,
-  onClose: mockDispatch,
+  isVisible: true,
+  onCloseHandler: () => {},
   title: 'Drawer Title',
 };
 
-const setup = (ownProps = props) => {
-  const {
-    getByTestId,
-    getByText,
-    queryByTestId,
-  } = render(<Drawer {...ownProps} />);
-
-  const container = getByTestId('drawer');
-
-  return {
-    container,
-    getByTestId,
-    getByText,
-    queryByTestId,
-  };
-};
-
 describe('Drawer', () => {
-  describe('basic functionality', () => {
-    it('it should be a render children', () => {
-      const { container } = setup();
-      expect(container).toMatchSnapshot();
-    });
+  it('it should render properly', () => {
+    const { container } = render(
+      <Drawer {...props} />
+    );
+    expect(container).toMatchSnapshot();
   });
 });
